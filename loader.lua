1a9071d2f3ed994c45c2a5b245a93374edf77e32
bapple4343 <throwawaybutthicc@gmail.com>
2024-10-21 19:42:17 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,4 @@
-
+if game.PlaceId == 3101667897 -- legends of speed
+    loadstring(game:HttpGet("legends of speed"))()
+else -- universal script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bapple4343/LuxHub/refs/heads/main/games/universal"))()
