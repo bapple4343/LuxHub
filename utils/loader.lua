3fb7809ebcf99c366d6bdc3ac24d30a42d0c8842
bapple4343 <throwawaybutthicc@gmail.com>
2024-10-23 19:59:06 +0200

    Update loader.lua

--- utils/loader.lua
+++ utils/loader.lua
@@ -1,7 +1,16 @@
--- welcome curious passerby
+if _G.luxloaded then
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	    Title = "Lux already loaded",
+	    Text = "Use the 'End' key to toggle the menu"
+    })
+    return
+end
+_G.luxloaded = true
 
 if game.PlaceId == 3101667897 then -- legends of speed
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bapple4343/LuxHub/refs/heads/main/games/legends_of_speed.lua"))()
+elseif game.PlaceId == 574407221 then -- super hero tycoon
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bapple4343/LuxHub/refs/heads/main/games/super_hero_tycoon.lua"))()
 else -- universal
     loadstring(game:HttpGet("https://raw.githubusercontent.com/bapple4343/LuxHub/refs/heads/main/games/universal"))()
 end
